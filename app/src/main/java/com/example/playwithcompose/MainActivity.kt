1707b17b12d439682291242614b44ad5d79adb41
Benjamin Orsini <benjamin.orsini@appaloosa-store.com>
2020-06-08 10:48:25 +0200

    feat: Initial activity

---
+++ app/src/main/java/com/example/playwithcompose/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.playwithcompose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.tooling.preview.Preview
+import com.example.playwithcompose.ui.AppTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppTheme {
+        Greeting("Android")
+    }
+}
